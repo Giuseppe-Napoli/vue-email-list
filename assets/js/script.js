db9f8685181626eea5581b85fa7d4762b5b9dab5
Giuseppe Napoli <giuseppe.napoli1993@hotmail.it>
2021-05-17 14:46:26 +0200

    alternativa con function()

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,22 +7,25 @@ const app = new Vue({
 
   mounted(){
 
-    for(let i = 0; i < 10; i++){
-
-      axios.get(' https://flynn.boolean.careers/exercises/api/random/mail')
-      .then (elem => {
-       // console.log(elem.data.response);
-        this.eMails.push(elem.data.response)
-      })
-      .catch(err =>{
-        console.log(err);
-      })
-    }
+    this.callApi(' https://flynn.boolean.careers/exercises/api/random/mail')
 
   },
   
   methods:{
     
+    callApi(url){
+      axios.get(url)
+      .then (elem => {
+       // console.log(elem.data.response);
+        this.eMails.push(elem.data.response)
+        if(this.eMails.length < 10){
+          this.callApi(url)
+        }
+      })
+      .catch(err =>{
+        console.log(err);
+      })
+    }
   }
 
 
